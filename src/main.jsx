e31b09b9703a471ad521855263940ffd760970c9
Karthik Aravind <karthikaravindpmc@gmail.com>
2024-04-24 15:58:32 +0530

    mui added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import App from "./App.jsx";
 
 import { PollProvider } from "./context/index.jsx";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
