296d34771f56266f68552079c0411e87cd270193
soundar-surya <bittergourd76@gmail.com>
2024-04-24 17:29:08 +0530

    merge auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Auth } from "./components/Auth";
 
 import { useContext, useEffect } from "react";
-// import { signInWithGooglePopup, auth } from "./firebase.utils"
+import { signInWithGooglePopup, auth } from "./firebase.utils"
 import { signOut, onAuthStateChanged } from "firebase/auth";
 
 import { PollContext } from "./context";
@@ -9,7 +9,45 @@ import { PollContext } from "./context";
 import "./App.css";
 
 function App() {
-  return <Auth />;
+
+  const { 
+    login,
+    logout,
+    state
+   } = useContext(PollContext);
+
+   useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      user && login(user.uid);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+
+  const signInUser = async () => {
+    try {
+      const response = await signInWithGooglePopup();
+      console.log("USER SIGNED IN");
+      login(response.user.uid);
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const signOutUser = async () => {
+    try {
+      const response = await signOut(auth);
+      console.log("USER SIGNEFD OUT", {response});
+      logout();
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Auth />
+  )
 }
 
-export default App;
+export default App
