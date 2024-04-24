7f6e7882fccbb72c8dcd8dfffbb23fb6678a1ff7
notAravind <140076135+notAravind@users.noreply.github.com>
2024-04-24 16:11:53 +0530

    Adding Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 
 import { PollProvider } from "./context/index.jsx";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import ReportPage from "./report_page.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <PollProvider>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/:questionID/" element={<ReportPage />} />
+          <Route path="/:questionID/report" element={<ReportPage />} />
+        </Routes>
       </PollProvider>
     </BrowserRouter>
   </React.StrictMode>
